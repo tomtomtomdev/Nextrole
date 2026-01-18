daff1afd6280f210f95d2a18e715f2afd8bc7eb2
tom <tommy.yohanes@icloud.com>
2026-01-18 19:58:50 +0700

    Initial Commit

---
+++ Nextrole/NextroleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextroleApp.swift
+//  Nextrole
+//
+//  Created by tomtomtom on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct NextroleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NextroleTests/NextroleTests.swift
@@ -0,0 +1,17 @@
+//
+//  NextroleTests.swift
+//  NextroleTests
+//
+//  Created by tomtomtom on 1/18/26.
+//
+
+import Testing
+@testable import Nextrole
+
+struct NextroleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
